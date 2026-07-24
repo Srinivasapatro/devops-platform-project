e7f39c937232fb6f5b1dc95782a72df72bf5d89d
Srinivasapatro <srinivasa_patro@outlook.com>
2026-07-24 17:29:36 +0530

    Add application package and unit tests

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+from app.app import app
+
+client = app.test_client()
+
+def test_home():
+    response = client.get("/")
+
+    assert response.status_code == 200
+
+    data = response.get_json()
+
+    assert data["application"] == "DevOps Platform Project"
+    assert data["status"] == "running"
